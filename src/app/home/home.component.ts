80c6ce82523eeb224838ffe4a064414d16b20fbd
Kevin <kevin@kevin.home>
2018-04-16 12:55:31 -0500

    finished goals app

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,32 +9,31 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
   animations: [
     trigger('goals', [
       transition('* => *', [
-        query(':enter', style({ opacity: 0 }), {optional: true}),
+        query(':enter', style({ opacity: 0 }), { optional: true }),
 
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
           ]))]), { optional: true }),
-        
+
         query(':leave', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+            style({ opacity: .5, transform: 'translateY(35px)', offset: .3 }),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 }),
           ]))]), { optional: true })
       ])
-    ])
-
-  ]
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number = 4;
   btnText: string = "Add an item";
-  goalText: string = 'My first life goal';
-  goals = ['My first life goal', 'I want to climb a mountain', 'I want to go ice skating'];
+  goalText: string = '';
+  goals = ['Click here to remove goal'];
 
   constructor() { }
 
